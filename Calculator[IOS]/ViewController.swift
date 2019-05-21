69e4cee0e78223de27eead47cdb7e7f710caa6b9
Deshpande, Anup <adeshp16@cci0bhgq18aws.local>
2019-05-21 17:51:07 -0400

    Initial Commit

--- Calculator[IOS]/ViewController.swift
+++ Calculator[IOS]/ViewController.swift
@@ -10,11 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBOutlet weak var operations: UISegmentedControl!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        if let sender = sender.title(for: UIControl.State.normal){
+            if sender == "Clear All"{
+                firstNumber.text = ""
+                secondNumber.text = ""
+                resultLabel.text = "Result: 0.00"
+            }
+            
+            if sender == "Perform Operation"{
+                let isValid = validateInputs()
+                if (isValid){
+                    
+                    let num1 = Double(firstNumber.text!)
+                    let num2 = Double(secondNumber.text!)
+                    
+                    
+                    switch operations.selectedSegmentIndex{
+                    case 0:
+                        resultLabel.text = "Result: \(num1! + num2!)"
+                    case 1:
+                        resultLabel.text = "Result: \(num1! - num2!)"
+                    case 2:
+                        resultLabel.text = "Result: \(num1! * num2!)"
+                    case 3:
+                        if num2 != 0{
+                            resultLabel.text = "Result: \(num1! / num2!)"
+                        }else{
+                            let alert = UIAlertController(title: "Warning", message: "Divide by zero produces Infinity", preferredStyle: .alert)
+                            let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+                            alert.addAction(action)
+                            present(alert,animated: true,completion: nil)
+                        }
+                    default:
+                        print("Default statement")
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func validateInputs() -> Bool{
+        
+       if(firstNumber.text == "" && secondNumber.text == ""){
+        
+        let alert = UIAlertController(title: "Warning", message: "Please enter numbers", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+        return false
+        
+        }
+       else if(firstNumber.text != "" && secondNumber.text == ""){
+            let alert = UIAlertController(title: "Warning", message: "Please Enter Second Number", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+            alert.addAction(action)
+            present(alert,animated: true,completion: nil)
+            return false
+        }
+       else if(firstNumber.text == "" && secondNumber.text != ""){
+        
+        
+        let alert = UIAlertController(title: "Warning", message: "Please Enter First Number", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert,animated: true,completion: nil)
+        return false
+        }
+        return true
+    }
 }
 
